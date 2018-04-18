7d7c922d0cb13653a147cf19bb3cc674b8c07d80
Subhranil Dey <subhranildey@users.noreply.github.com>
2018-04-18 17:27:29 +0530

    Update ec2-list.sh

--- ec2-list.sh
+++ ec2-list.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 describe-instances --output text   --query 'Reservations[].Instances[].[Tags[?Key==`Name`] | [0].Value, PublicIpAddress]' | grep -v None | sort | awk -v 'OFS=\t' '{print $1,$2}'
+aws ec2 describe-instances --output text --query 'Reservations[].Instances[].[Tags[?Key==`Name`] | [0].Value, PublicIpAddress]' | grep -v None | sort | awk -v 'OFS=\t' '{print $1,$2}'
